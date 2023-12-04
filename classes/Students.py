69c3df7a03668e1e95652a51d45e74b5fa401502
Filip Kotynia <147401244+kotyniafilip@users.noreply.github.com>
2023-12-04 16:25:18 +0100

    add new file

---
+++ classes/Students.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Student:
+    def __init__ (self,name, marks):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self) -> bool:
+        srednia = sum(self.marks) / len(self.marks)
+        return srednia > 50
