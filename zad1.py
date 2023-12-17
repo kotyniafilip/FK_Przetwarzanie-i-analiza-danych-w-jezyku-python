b60a055b207b9088c5da8b2a246737c04a37ac98
Filip Kotynia <147401244+kotyniafilip@users.noreply.github.com>
2023-12-17 14:49:35 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__ (self,name, marks):
+    def __init__(self, name, marks):
         self.name = name
         self.marks = marks
 
@@ -7,8 +7,8 @@ class Student:
         srednia = sum(self.marks) / len(self.marks)
         return srednia > 50
 
-student1 = Student("Kamil Noga", [60,70,80,90])
-student2 = Student("Kamil Slimak", [20,30,40,25])
+student1 = Student("Kamil Noga", [60, 70, 80, 90])
+student2 = Student("Kamil Slimak", [20, 30, 40, 25])
 
 wynik1 = student1.is_passed()
 wynik2 = student2.is_passed()
