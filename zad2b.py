fef04f96c00237e49a77030ba9ed02a619778c2e
Filip Kotynia <147401244+kotyniafilip@users.noreply.github.com>
2023-12-17 14:41:17 +0100

    Update zad2b.py

--- zad2b.py
+++ zad2b.py
@@ -3,6 +3,7 @@ def x2numbers(list_of_numbers):
     for el in list_of_numbers:
         numbers.append(el * 2)
     return numbers
-list = [5,10,15,20,25]
-numbers = x2numbers(list)
-print(numbers)
+
+my_list = [5, 10, 15, 20, 25]
+result_numbers = x2numbers(my_list)
+print(result_numbers)
