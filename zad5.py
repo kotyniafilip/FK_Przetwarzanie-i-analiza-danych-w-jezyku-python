1b6dacfb50788a2cd7d860e2ab35562a799e6e7e
Filip Kotynia <147401244+kotyniafilip@users.noreply.github.com>
2023-12-17 14:46:57 +0100

    Update zad5.py

--- zad5.py
+++ zad5.py
@@ -4,7 +4,7 @@ def czy_jest_wartosc(lista: list, wartosc: int) -> bool:
     else:
         return False
 
-moja_lista = [1,2,3,4,5,6,7]
+moja_lista = [1, 2, 3, 4, 5, 6, 7]
 moja_liczba = 10
 
 wynik = czy_jest_wartosc(moja_lista, moja_liczba)
